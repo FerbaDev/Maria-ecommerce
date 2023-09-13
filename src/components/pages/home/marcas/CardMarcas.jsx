4959bb5865c90aca747f0354ce53a2250dd0b909
FerbaDev <elferbarron1@gmail.com>
2023-09-13 01:45:08 -0300

    se le da funcionalidad a las cards de marcas

--- src/components/pages/home/marcas/CardMarcas.jsx
+++ src/components/pages/home/marcas/CardMarcas.jsx
@@ -3,27 +3,30 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
+import { Link } from "react-router-dom";
 
-export const CardMarcas = ({ nombre, descripcion, img }) => {
+export const CardMarcas = ({ nombre, descripcion, img, path }) => {
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image={img}
-          alt={nombre}
-          style={{ objectFit: "contain", padding: "20px" }}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {nombre}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {descripcion}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <Link to={path}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="140"
+            image={img}
+            alt={nombre}
+            style={{ objectFit: "contain", padding: "20px" }}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {nombre}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {descripcion}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Link>
     </Card>
   );
 };
