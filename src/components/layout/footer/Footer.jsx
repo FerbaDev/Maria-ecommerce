529ad6209b81de351d2d9411d7228f6b62dfd900
FerbaDev <elferbarron1@gmail.com>
2023-11-08 11:48:35 -0300

    se cambia cursor en mensaje a whatsapp en footer

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 import ReactWhatsapp from "react-whatsapp";
+import { Box } from "@mui/material";
 
 export const Footer = () => {
   return (
@@ -9,8 +10,13 @@ export const Footer = () => {
         number="549-3571-52-1084"
         message="Hola María! Me gustaría hacer una consulta."
         element="div"
+        style={{ display: "flex", gap: "5px" }}
+        className="cursor-manito"
       >
         <WhatsAppIcon />
+        <Box sx={{ display: { xs: "none", sm: "block" } }}>
+          <p>¡Hola María!</p>
+        </Box>
       </ReactWhatsapp>
       <p style={{ fontFamily: "monospace" }}>
         Tienda creada por
