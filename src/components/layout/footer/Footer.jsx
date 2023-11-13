b8fea1070592cef1c6cc4b8c685e2c48674c4b71
eritalla <erica.tallapietra@gmail.com>
2023-11-13 20:12:00 -0300

    se trabaja en footer

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -9,11 +9,11 @@ export const Footer = () => {
   return (
    <>
     <Box sx={{minHeight: "300px", backgroundColor: "#1c1c1c", color: "whitesmoke"}}>
-      <Box sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+      <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", paddingBlock: "20px"}} >
         <Typography variant="h5" component="h2" align="center">
           Nos encontrás en:
         </Typography>
-        <Box sx={{display: "flex", flexDirection: "row", gap: "1.5em"}} p={2}>
+        <Box sx={{display: "flex", flexDirection: "row", gap: "2em", }} p={2}>
 
         <ReactWhatsapp
         number="549-3571-52-1084"
@@ -22,15 +22,26 @@ export const Footer = () => {
         className="cursor-manito"
       >
         <WhatsAppIcon style={{fontSize: "3em"}}/>
-        
-       
       </ReactWhatsapp>
-      <InstagramIcon style={{fontSize: "3em"}}/>
+      <Link to={"https://www.instagram.com/mariashoesclothes/" } style={{color: "whitesmoke"}}>
+        <InstagramIcon style={{fontSize: "3em"}}/>
+      </Link>
+      <Link to={"https://wwww.facebook.com/mariashoesandclotes"} style={{color: "whitesmoke"}}>
         <FacebookIcon style={{fontSize: "3em"}}/>
+      </Link>
         </Box>
       </Box>
-      
+      <Box>
+      <Typography variant="h5" component="h2" align="center">
+          Dirección:
+        </Typography>
+        <Typography variant="h6" component="h2" align="center">
+          Velez Sarsfield 116, Río Tercero, Córdoba.
+        </Typography>
+      </Box>
     </Box>
+
+    {/* footer talba  */}
     <Box sx={{backgroundColor: "black", width: "100%", display: "flex", justifyContent: "end", p: 2, color: "whitesmoke"}}>
 
     <p style={{ fontFamily: "monospace" }}>
